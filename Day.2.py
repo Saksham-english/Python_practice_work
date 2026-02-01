d7f2d503f04511adb33bd7562c9c2fd7744102a5
Saksham-english <rahulkaushil01@gmail.com>
2026-02-01 18:40:03 +0530

    .

---
+++ Day.2.py
@@ -0,0 +1,18 @@
+# basic calculator 
+a=int(input("Enter a number a :"))
+b=int(input("Enter a number b :"))
+Add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+if a==0:
+    print("Division by zero is not allowed")
+else :
+     div=a/b
+
+
+print("The addition of a and b is :",Add)
+print("The subtraction of a and b is :",sub)
+print("The multiplication of a and b is :",mul)
+print("The division of a and b is :",div)
